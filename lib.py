5920fbfd06c9e888e241d38c9b4ca3a1c98f9d0e
Michael Peters <opiethehokie@gmail.com>
2020-04-13 22:31:39 -0400

    adds volume bars approximation

--- lib.py
+++ lib.py
@@ -1,7 +1,12 @@
+import os
+import sys
 import warnings
 
 from datetime import date, timedelta
+
+sys.stdout = open(os.devnull, "w")
 from mlfinlab.data_structures import standard_data_structures
+sys.stdout = sys.__stdout__
 
 import pandas as pd
 import requests_cache
@@ -12,24 +17,26 @@ from pandas_datareader import data as pdr
 
 yf.pdr_override()
 
-def get_daily_returns(tickers, start, end):
+def get_daily_returns(tickers, start, end, log=False):
   session = requests_cache.CachedSession(backend='sqlite', expire_after=timedelta(days=1))
   data = pdr.get_data_yahoo(tickers, start=start, end=end, session=session, auto_adjust=True)
   close = data['Close']
   close.index = pd.to_datetime(close.index)
-  returns = close.pct_change(1).dropna() # https://mathbabe.org/2011/08/30/why-log-returns/
-  return returns
+  returns = pd.log(close).diff() if log else close.pct_change(1) # https://mathbabe.org/2011/08/30/why-log-returns/
+  return returns.dropna()
 
-def get_volume_bars(tickers, start, end):
+def get_volume_bars(tickers, start, end, log=False):
   session = requests_cache.CachedSession(backend='sqlite', expire_after=timedelta(days=1))
-  data = pdr.get_data_yahoo(tickers, start=start, end=end, session=session, interval='15m', auto_adjust=True)
-  data.reset_index(level=0, inplace=True)
-  data = pd.concat([data['Datetime'], data['Close'], data['Volume']], axis=1)
-  data.columns = ['date', 'price', 'volume']
-  bars = standard_data_structures.get_volume_bars(data, threshold=2000000, verbose=True)
-  print(bars)
+  # use S&P 500 as a volume proxy instead of tring to combine basket of volume bars
+  market_proxy_data = pdr.get_data_yahoo('SPY', start=start, end=end, session=session, interval='5m', auto_adjust=True)
+  market_proxy_data.reset_index(level=0, inplace=True)
+  market_proxy_data = pd.concat([market_proxy_data['Datetime'], market_proxy_data['Close'], market_proxy_data['Volume']], axis=1)
+  market_proxy_data.columns = ['date', 'price', 'volume']
+  avg_market_proxy_daily_volume = market_proxy_data['volume'].sum() / 59
+  market_proxy_bars = standard_data_structures.get_volume_bars(market_proxy_data, threshold=avg_market_proxy_daily_volume, verbose=False)
+  data = pdr.get_data_yahoo(tickers, start=start, end=end, session=session, interval='5m', auto_adjust=True)
+  # sample tickers using SPY's volume bar date times
+  bars = data[data.index.isin(market_proxy_bars['date_time'])]
+  returns = pd.log(bars['Close']).diff() if log else bars['Close'].pct_change(1)
+  return returns.dropna()
   
-
-from datetime import date, timedelta
-from dateutil.relativedelta import relativedelta
-get_volume_bars(['VTI'], date.today() + relativedelta(days=-59), date.today())
