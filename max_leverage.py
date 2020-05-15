ae899a3b25e96b680672e9d035b544837b287570
Michael Peters <opiethehokie@gmail.com>
2020-05-14 23:04:30 -0400

    adds fractional kelly and leverage target

--- max_leverage.py
+++ max_leverage.py
@@ -1,3 +1,5 @@
+import pprint
+
 import numpy as np
 
 from datetime import date
@@ -5,7 +7,7 @@ from dateutil.relativedelta import relativedelta
 
 from lib import get_daily_returns
 
-trading_days_per_year = 251
+trading_days_per_year = 252
 daily_risk_free_rate = .0011 / trading_days_per_year # daily 3-month treasury rate
 
 # http://ddnum.com/articles/leveragedETFs.php
@@ -13,32 +15,34 @@ daily_risk_free_rate = .0011 / trading_days_per_year # daily 3-month treasury ra
 # https://blog.thinknewfound.com/2018/01/levered-etfs-long-run/
 def kelly_max_leverage(returns):
     returns -= daily_risk_free_rate
-    mean_daily_returns = returns.mean(axis=0) * trading_days_per_year
+    mean_daily_returns = returns.mean() * trading_days_per_year
     var = returns.var() * trading_days_per_year
     return mean_daily_returns / var # ignores skewness and kurtosis
 
 # https://epchan.blogspot.com/2014/08/kelly-vs-markowitz-portfolio.html?m=1
-def kelly_weight_optimization(returns):
+def kelly_weight_optimization(returns, fraction=None, target_leverage=None):
     returns -= daily_risk_free_rate
     C = returns.cov() * trading_days_per_year
     M = returns.mean() * trading_days_per_year
     F = np.linalg.inv(C).dot(M)
+    if fraction:
+        F *= fraction
+    if target_leverage:
+        F = [i / sum(np.absolute(F)) * target_leverage for i in F]
     return { ticker : leverage for ticker, leverage in zip(returns.columns.values.tolist(), F)}
 
-tickers = ['VOO', 'BND']
+tickers = ['VTI','VXUS','BND','BNDX']
 
 # constant - should still rebalance at least quarterly -> monthly
 returns = get_daily_returns(tickers, date.today() + relativedelta(months=-60), date.today())
 print(kelly_max_leverage(returns))
 
-weights = kelly_weight_optimization(returns)
-print(weights)
-print(sum(weights.values()))
+weights = kelly_weight_optimization(returns, fraction=.5)
+pprint.pprint(weights)
 
 # dynamic based on volatility
 returns = get_daily_returns(tickers, date.today() + relativedelta(days=-60), date.today())
 print(kelly_max_leverage(returns))
 
-weights = kelly_weight_optimization(returns)
-print(weights)
-print(sum(weights.values()))
+weights = kelly_weight_optimization(returns, target_leverage=1)
+pprint.pprint(weights)
